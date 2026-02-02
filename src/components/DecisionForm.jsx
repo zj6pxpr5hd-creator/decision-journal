8adfad4d571da1dd306c2184340b36dfbd8980c7
edozzz06 <edoardovolley06@gmail.com>
2026-02-02 19:15:01 +0100

    added header, form and list files

---
+++ src/components/DecisionForm.jsx
@@ -0,0 +1,15 @@
+function DecisionForm(){
+
+    return(
+        <section className="form-container">
+            <h2>Decision Form</h2>
+            <input type="text" placeholder="Title"/>
+            <input type="text" placeholder="Reason"/>
+            <input type="text" placeholder="Confidence Level"/>
+        </section>
+    )
+
+
+}
+
+export default DecisionForm;
---
+++ src/components/DecisionList.jsx
@@ -0,0 +1,16 @@
+function  DecisionList(){
+
+    return(
+        <section>
+            <h2>Decision List</h2>
+            <ol>
+                <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum beatae iure ratione magni reprehenderit! Quam sunt quae sed quo incidunt, libero, assumenda officiis repellendus dignissimos iusto ipsum velit animi corporis?</li>
+                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis magnam repellat, saepe dolorem maxime delectus eos voluptatum tempore consequuntur corrupti aliquid expedita! Libero, eius dolores at architecto in doloribus error.</li>
+            </ol>
+        </section>
+    )
+
+}
+
+
+export default DecisionList;
