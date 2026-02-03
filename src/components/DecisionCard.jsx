3a1d903c4ecdba3ca70bbffa4526607333f0ec45
edozzz06 <edoardovolley06@gmail.com>
2026-02-03 19:23:01 +0100

    added styling

--- src/components/DecisionCard.jsx
+++ src/components/DecisionCard.jsx
@@ -2,11 +2,12 @@ function DecisionCard({ decision }){
     return(
     <div className="decision-card">
         <h3>{decision.title}</h3>
-        <h4 className="confidence-section">Confidence Level: {decision.confidence}</h4>
-        <div className="reason-section">
-            <h3>Reason: </h3> <p>{decision.reason}</p>
+        <div className="confidence-section">
+            <p><span className="small-title">Confidence level:</span>    {decision.confidence}</p>
         </div>
-
+        <div className="reason-section">
+            <p><span className="small-title">Reason:</span>    {decision.reason}</p>
+        </div>
 
     </div>
     )
